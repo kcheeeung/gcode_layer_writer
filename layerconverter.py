1c45b0671d15444fccf967b2a2f615ab6fe66415
Katie Wu <katielwu@berkeley.edu>
2017-02-11 13:28:31 -0800

    json read and write

--- layerconverter.py
+++ layerconverter.py
@@ -1,3 +1,13 @@
-data = {x:0, y:0, z:0, pwm:0, unitsize:0, folder:"test_layers"}
-with open('config.json', "w") as fp:
-	json.dump(data, fp)
+import json
+
+def write_json(filename):
+	data = {x:0, y:0, z:0, pwm:0, unitsize:0, folder:"test_layers"}
+	with open(filename, "w") as fp:
+		json.dump(data, fp)
+
+def load_json(filename):
+	with open(filename, "r") as fp:
+		data = json.load(fp)
+
+write_json("config.json")
+load_json("config.json")
